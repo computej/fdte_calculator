156486e1386b56f65a83285d05c83444908fe455
computej <computejpdc@gmail.com>
2023-07-20 10:47:12 -0700

    display results for difference, product, and quotient

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
+function saySomething(value1, value2) {
+  window.alert("The sum is " + add(value1 ,value2) + ".");
+  window.alert("The difference is " + subtract(value1 ,value2) + ".");
+  window.alert("The product is " + multiply(value1 ,value2) + ".");
+  window.alert("The quotient is " + divide(value1 ,value2) + ".");
 }
 
 function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1,number2) {
+  return number1 - number2;
+}
+
+function multiply(number1,number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 const additionNumber1 = Number(window.prompt("Enter a number:"));
 const additionNumber2 = Number(window.prompt("Enter another number:"));
 
-saySomething("The sum is " + add(additionNumber1 ,additionNumber2) + ".");
+saySomething(additionNumber1, additionNumber2);
